f93b90097608b10245264295e8f4e1a6c6c4bb72
calvinAE <58550080+calvinAE@users.noreply.github.com>
2022-11-29 11:13:02 +0100

    Bottom navigation

--- app/src/main/java/com/example/apexify/MainActivity.kt
+++ app/src/main/java/com/example/apexify/MainActivity.kt
@@ -2,12 +2,45 @@ package com.example.apexify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.apexify.Fragments.MapFragment
+import com.example.apexify.Fragments.NewsFragment
+import com.example.apexify.Fragments.StatsFragment
+import com.example.apexify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Set map and check map icon
+        replaceFragment(MapFragment())
+        binding.bottomNavigation.menu.findItem(R.id.Map).setChecked(true)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.Map -> replaceFragment(MapFragment())
+                R.id.News -> replaceFragment(NewsFragment())
+                R.id.Stats -> replaceFragment(StatsFragment())
+
+                else ->{
+                }
+            }
+            true
+        }
     }
 
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
 
 }
