dc766b8ccb9699a5e60b40ed77c0cb1966062544
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-01 19:24:57 +0100

    Added store fragment

---
+++ app/src/main/java/com/example/apexify/Fragments/StoreFragment.kt
@@ -0,0 +1,77 @@
+package com.example.apexify.Fragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apexify.ApiInterface
+import com.example.apexify.Model.Store
+import com.example.apexify.Model.StoreItem
+import com.example.apexify.R
+import com.example.apexify.StoreAdapter
+import com.example.apexify.databinding.FragmentStoreBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class StoreFragment: Fragment(R.layout.fragment_store) {
+    private var _binding  : FragmentStoreBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var storeList: ArrayList<StoreItem>
+
+
+    private var auth = "19319beea4f9eb0f896dd0d5caa1793e"
+
+    override fun onCreateView(
+        layoutInflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStoreBinding.inflate(layoutInflater, container, false)
+        val view = binding.root
+
+        val recyclerView: RecyclerView = binding.rvStore
+        storeList = ArrayList()
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL ,false)
+        val storeAdapter = StoreAdapter(storeList)
+
+
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://api.mozambiquehe.re/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        val apiInterface: ApiInterface = retrofit.create(ApiInterface::class.java)
+
+        val call: Call<Store> = apiInterface.fetchStore()
+
+        call.enqueue(object : Callback<Store?> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(call: Call<Store?>, response: Response<Store?>) {
+                storeList.clear()
+                for(news in response.body()!!){
+                    storeList.add(news)
+                }
+                storeAdapter.notifyDataSetChanged()
+
+                recyclerView.adapter = storeAdapter
+
+            }
+
+            override fun onFailure(call: Call<Store?>, t: Throwable) {
+              println(t)
+            }
+        })
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/apexify/StoreAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.apexify
+
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apexify.Model.StoreItem
+import com.squareup.picasso.Picasso
+
+class StoreAdapter(private val dataSet: List<StoreItem>)  : RecyclerView.Adapter<StoreAdapter.ViewHolder>()  {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val tv_title: TextView
+        val tv_price: TextView
+        val img_store: ImageView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            tv_title = view.findViewById(R.id.tv_title)
+            tv_price = view.findViewById(R.id.tv_price)
+            img_store = view.findViewById(R.id.img_store)
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.store_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StoreAdapter.ViewHolder, position: Int) {
+        val item = dataSet[position]
+        val price =  item.pricing.map { it.quantity}.toString() +  item.pricing.map { it.ref }.toString()
+
+
+        holder.apply {
+            tv_title.text = item.title.toString()
+            Picasso.get().load(item.asset).into(img_store)
+            tv_price.text = price.replace("[", "").replace("]", " ");
+        }
+    }
+
+    override fun getItemCount(): Int = dataSet.size
+
+
+
+}
