e4dcce3f27a6c3fd5d6816a435f53cc6cf7de76e
calvinAE <58550080+calvinAE@users.noreply.github.com>
2023-01-01 19:25:13 +0100

    Update NewsAdapter.kt

--- app/src/main/java/com/example/apexify/NewsAdapter.kt
+++ app/src/main/java/com/example/apexify/NewsAdapter.kt
@@ -1,22 +1,32 @@
 package com.example.apexify
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.apexify.Model.NewsItem
+import com.example.apexify.Model.StoreItem
+import com.squareup.picasso.Picasso
 
-class NewsAdapter(private val dataSet: List<String>)  : RecyclerView.Adapter<NewsAdapter.ViewHolder>()  {
+
+class NewsAdapter(private val dataSet: ArrayList<NewsItem>)  : RecyclerView.Adapter<NewsAdapter.ViewHolder>()  {
 
         class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val textView: TextView
-            val imageView: ImageView
+            val tv_news: TextView
+            val img_news: ImageView
+            val tv_shortDesc: TextView
 
             init {
                 // Define click listener for the ViewHolder's View.
-                textView = view.findViewById(R.id.tv_news)
-                imageView = view.findViewById(R.id.img_news)
+                tv_news = view.findViewById(R.id.tv_news)
+                img_news = view.findViewById(R.id.img_news)
+                tv_shortDesc = view.findViewById(R.id.tv_shortDesc)
+
             }
         }
 
@@ -24,12 +34,23 @@ class NewsAdapter(private val dataSet: List<String>)  : RecyclerView.Adapter<New
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.news_item, viewGroup, false)
-
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.textView.text = dataSet[position]
+       val item = dataSet[position]
+
+        //Open browser to navigate to the link
+        viewHolder.itemView.setOnClickListener {
+            val browser = Intent(Intent.ACTION_VIEW, Uri.parse(item.link))
+            viewHolder.itemView.context.startActivity(browser)
+
+        }
+        viewHolder.apply {
+            tv_news.text = item.title.toString()
+            Picasso.get().load(item.img).into(img_news)
+            tv_shortDesc.text = item.short_desc.toString()
+        }
     }
 
     override fun getItemCount() = dataSet.size
